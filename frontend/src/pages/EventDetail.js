ca88c51a81958f7ed82f39b1712d17a26a92fbac
Rohit Singh <rohitsinghpb4@gmail.com>
2024-06-15 14:07:17 +0530

    fetch events data

--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,13 +1,27 @@
-import { useParams } from "react-router-dom";
+import { useParams, json, useLoaderData } from "react-router-dom";
+import EventItem from "../components/EventItem";
 
 function EventDetailPage() {
-    const param = useParams();
+    // const param = useParams();
+    // const id = params.eventId;
 
-    return (
-        <>
-            <h1>Event Detail Page</h1>
-            <p>Event id: {param.eventId}</p>
-        </>
-    );
+    const data = useLoaderData();
+
+    return <EventItem event={data.event} />;
 }
 export default EventDetailPage;
+
+export async function loader({ request, params }) {
+    const id = params.eventId;
+
+    const response = await fetch("http://locahost:8080/event/" + id);
+
+    if (!response.ok) {
+        throw json(
+            { message: "Could not fetch details for selected event." },
+            { status: 500 }
+        );
+    } else {
+        return response;
+    }
+}
