2f78adf5d4861fad484dc5b8bd9945f5de0d61e9
Rohit Singh <rohitsinghpb4@gmail.com>
2024-06-16 21:51:56 +0530

    added client side validations

--- frontend/src/components/EventForm.js
+++ frontend/src/components/EventForm.js
@@ -1,8 +1,14 @@
-import { useNavigate, Form, useNavigation } from "react-router-dom";
+import {
+    useNavigate,
+    Form,
+    useNavigation,
+    useActionData,
+} from "react-router-dom";
 
 import classes from "./EventForm.module.css";
 
 function EventForm({ method, event }) {
+    const data = useActionData();
     const navigate = useNavigate();
     const navigation = useNavigation();
 
@@ -14,13 +20,20 @@ function EventForm({ method, event }) {
 
     return (
         <Form method="post" className={classes.form}>
+            {data && data.errors && (
+                <ul>
+                    {Object.values(data.errors).map((err) => (
+                        <li key={err}>{err}</li>
+                    ))}
+                </ul>
+            )}
             <p>
                 <label htmlFor="title">Title</label>
                 <input
                     id="title"
                     type="text"
                     name="title"
-                    required
+                    // required
                     defaultValue={event ? event.title : ""}
                 />
             </p>
@@ -30,7 +43,7 @@ function EventForm({ method, event }) {
                     id="image"
                     type="url"
                     name="image"
-                    required
+                    // required
                     defaultValue={event ? event.image : ""}
                 />
             </p>
@@ -40,7 +53,7 @@ function EventForm({ method, event }) {
                     id="date"
                     type="date"
                     name="date"
-                    required
+                    // required
                     defaultValue={event ? event.date : ""}
                 />
             </p>
@@ -50,7 +63,7 @@ function EventForm({ method, event }) {
                     id="description"
                     name="description"
                     rows="5"
-                    required
+                    // required
                     defaultValue={event ? event.description : ""}
                 />
             </p>
