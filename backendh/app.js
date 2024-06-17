f18ace52c0cd2009dfad3a9a50eb2449af27ac22
Rohit Singh <rohitsinghpb4@gmail.com>
2024-06-17 22:28:43 +0530

    backend resetup

---
+++ backendh/app.js
@@ -0,0 +1,27 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const cors = require("cors");
+
+const eventRoutes = require("./routes/events");
+
+const app = express();
+
+app.use(cors());
+
+app.use(bodyParser.json());
+// app.use((req, res, next) => {
+//     res.setHeader("Access-Control-Allow-Origin", "*");
+//     res.setHeader("Access-Control-Allow-Methods", "*");
+//     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+//     next();
+// });
+
+app.use("/events", eventRoutes);
+
+app.use((error, req, res, next) => {
+    const status = error.status || 500;
+    const message = error.message || "Something went wrong.";
+    res.status(status).json({ message: message });
+});
+
+app.listen(8080);
