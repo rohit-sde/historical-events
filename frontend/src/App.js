28f5bac390fd32935becae5cd0d892a55c59cd1c
Rohit Singh <rohitsinghpb4@gmail.com>
2024-06-17 17:21:54 +0530

    added in navigation bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,7 @@ import RootLayout from "./pages/Root";
 import ErrorPage from "./pages/Error";
 import { action as manipulateEventAction } from "./components/EventForm";
 import NewsletterPage, { action as newsletterAction } from "./pages/Newsletter";
+import AuthenticationPage from "./pages/Authentication";
 
 const router = createBrowserRouter([
     {
@@ -53,6 +54,10 @@ const router = createBrowserRouter([
                         action: manipulateEventAction,
                     },
                 ],
+            },
+            {
+                path: "auth",
+                element: <AuthenticationPage />,
             },
             {
                 path: "newsletter",
